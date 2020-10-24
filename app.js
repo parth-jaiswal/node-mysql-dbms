39f87edbe4a7f183d22d071e4bf28b45c7ce3764
parth-jaiswal <55652727+parth-jaiswal@users.noreply.github.com>
2020-10-24 12:24:26 +0530

    added backend and connected to database

---
+++ app.js
@@ -0,0 +1,87 @@
+const mysql = require('mysql');
+const express  = require ('express');
+const bodyParser  = require ('body-parser');
+const ejs  = require ('ejs');
+const { constant } = require('async');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+
+var con = mysql.createConnection({
+	host: "localhost",
+	user: "parthdb",
+  password: "",
+  database: "hotel"
+});
+
+
+
+con.connect(function (err) {
+	if (err) throw err;
+	console.log("Connected!");
+});
+
+
+app.get("/", (req,res)=>{
+  var sql = "select * from employee_details where emp_id=10001";
+	con.query(sql, (error, result, fields) => {
+		if (error) throw error;
+    //console.log(result);
+    res.render('index', {list: JSON.stringify(result)});
+	});
+})
+  
+ app.post("/", (req, res) => {
+
+  let cust = {
+    fname : req.body.fname,
+    mname : req.body.mname,
+    lname : req.body.lname,
+    idtype : req.body.idtype,
+    idno : req.body.idno,
+    checkin : req.body.checkin,
+    checkout : req.body.checkout,
+    roomType : req.body.roomType,
+    dob: req.body.dob,
+    gender : req.body.gender,
+    contact: req.body.contact,
+    email: req.body.email,
+    house: req.body.house,
+    city: req.body.city,
+    state: req.body.state,
+    country: req.body.country
+  }
+  for(i in cust) {
+    if (cust[i] == '') {
+      cust[i] = null
+    }
+  }
+  console.log(cust);
+
+  let insertCustomer = `insert into customer values (10006, '${cust.fname}', "${cust.mname}", "${cust.lname}", '${cust.dob}', '${cust.gender}')`;
+    con.query(insertCustomer, (error, result, fields) => {
+		  if (error) throw error;
+		//console.log(result);
+		console.log(`${cust.fname} inserted successfully`);
+  });
+  
+  let insertCustDetails = `insert into customer_details values (10006, '${cust.idtype}', "${cust.idno}", "${cust.contact}", '${cust.email}', '${cust.house}', '${cust.city}', '${cust.state}', '${cust.country}')`;
+  con.query(insertCustDetails, (error, result, fields) => {
+    if(error) throw error;
+    console.log(`${cust.fname} details entered`);
+  })
+})
+
+app.get('/admin', (req, res)=>{
+  let employeeQuery = 'select * from employee e inner join employee_details ed on e.emp_id = ed.emp_id';
+  con.query(employeeQuery, (error, result, fields) => {
+    if (error) throw error;
+    res.render('admin', {list: JSON.stringify(result)} );
+  })
+})
+app.listen(3000, ()=>{
+  console.log('Server started on port 3000');
+});
